b6886531217bbfb5e07dffe62af5e97aef3bc981
Mazhit Alfarabi <a-mazhit@mail.ru>
2023-08-14 00:52:19 +0500

    production mode configured

--- config/db.js
+++ config/db.js
@@ -1,15 +1,38 @@
 const { Sequelize } = require("sequelize");
 const config = require("./config");
-const sequelize = new Sequelize(
-  config.development.database,
-  config.development.username,
-  config.development.password,
-  {
-    host: config.development.host,
-    port: config.development.port,
-    dialect: config.development.dialect,
-  }
-);
+const fs = require("fs");
+const path = require("path");
+let sequelize;
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(
+    config.production.database,
+    config.production.username,
+    config.production.password,
+    {
+      host: config.production.host,
+      port: config.production.port,
+      dialect: config.production.dialect,
+      dialectOptions: {
+        ssl: {
+          ca: fs.readFileSync(path.resolve("config", "./ca-certificate (1).crt")),
+        },
+      },
+    }
+  );
+} else {
+  sequelize = new Sequelize(
+    config.development.database,
+    config.development.username,
+    config.development.password,
+    {
+      host: config.development.host,
+      port: config.development.port,
+      dialect: config.development.dialect,
+    }
+  );
+}
+
 sequelize
   .authenticate()
   .then(() => {
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Instagram Node JS Rest API
